d1ad94507733b535bf34528e26f360aecd49b6e9
Wendy Calderon <wendycalderon@gmail.com>
2018-09-12 02:39:23 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,4 +1,6 @@
+# Create a class and inherit from ActiveRecord::Migration
 
+# By convention, the class name should match the part of the
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and 
 # value types according to the spec
