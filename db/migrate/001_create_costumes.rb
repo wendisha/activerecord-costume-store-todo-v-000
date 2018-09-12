994d3c9a47edbd699862e2b9406a388cfd3dc1e4
Wendy Calderon <wendycalderon@gmail.com>
2018-09-12 02:42:51 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,6 +1,6 @@
 class CreateCostumes < ActiveRecord::Migration
   def change
-    create_table :artists do |t|
+    create_table :costumes do |t|
       t.string :name
       t.float :price
       t.string :size
