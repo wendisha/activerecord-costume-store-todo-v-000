e191f0627aef09a6b5b4eaacf8a618efb0b852eb
Wendy Calderon <wendycalderon@gmail.com>
2018-09-12 02:41:55 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,6 +1,3 @@
-# Define a change method in which to do the migration
-# In this change method, create columns with the correct names and 
-# value types according to the spec
 class CreateCostumes < ActiveRecord::Migration
   def change
     create_table :artists do |t|
