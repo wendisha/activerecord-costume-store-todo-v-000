03c83b0859800cb3e0672e4675406cb0c0f02ad8
Wendy Calderon <wendycalderon@gmail.com>
2018-12-07 03:54:22 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,11 +1,18 @@
+<<<<<<< HEAD
 class CreateCostumes < ActiveRecord::Migration[4.2]
+=======
+class CreateCostumes < ActiveRecord::Migration
+>>>>>>> 1a36c4f7f19e377be3d3fe0d5222f12e6dd48c4a
   def change
     create_table :costumes do |t|
       t.string :name
       t.float :price
       t.string :size
       t.string :image_url
+<<<<<<< HEAD
       t.timestamps 
+=======
+>>>>>>> 1a36c4f7f19e377be3d3fe0d5222f12e6dd48c4a
     end
   end
 end
